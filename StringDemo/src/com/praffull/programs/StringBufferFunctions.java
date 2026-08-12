03fc014c5d035c7147190fbda1033894873bd44b
SohamGlobal <sohamglobal@users.noreply.github.com>
2026-08-12 09:00:48 +0530

    Add files via upload

---
+++ StringDemo/src/com/praffull/programs/StringBufferFunctions.java
@@ -0,0 +1,24 @@
+package com.praffull.programs;
+
+public class StringBufferFunctions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer sb=new StringBuffer("technology");
+		System.out.println(sb);
+		sb.append(" is your future");
+		System.out.println(sb);
+		sb.insert(11, "with ai ");
+		System.out.println(sb);
+		sb.delete(5, 17);
+		System.out.println(sb);
+		sb.deleteCharAt(4);
+		System.out.println(sb);
+		sb.replace(3, 9, "123456");
+		System.out.println(sb);
+		sb.reverse();
+		System.out.println(sb);
+		
+	}
+
+}
