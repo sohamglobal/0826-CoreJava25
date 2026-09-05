5b590bbcb5033ec6478f29d6a71db9402bd3bda2
SohamGlobal <sohamglobal@users.noreply.github.com>
2026-09-05 09:39:11 +0530

    Add files via upload

--- FullObjectOriented/src/com/praffull/programs/Hospital.java
+++ FullObjectOriented/src/com/praffull/programs/Hospital.java
@@ -6,9 +6,18 @@ public class Hospital {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//Patient pat=new Patient();
-		//pat.showName();
-		Patient.showName();
+		Patient p1=new Patient();
+		p1.setName("James");
+		//Patient.showName();
+		p1.showName();
+		
+		Patient p2=new Patient();
+		p2.setName("Patrick");
+		p2.showName();
+		
+		p1.showName();
+		
+		
 	}
 
 }
--- FullObjectOriented/src/com/sohamglobal/classes/Patient.java
+++ FullObjectOriented/src/com/sohamglobal/classes/Patient.java
@@ -2,9 +2,18 @@ package com.sohamglobal.classes;
 
 public class Patient {
 	
+	private static String name;
+	private String city;
+			
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public static void showName()
 	{
-		System.out.println("Patient name is John Wick");
+		System.out.println("Patient name is "+name);
 	}
+	
+	
 
 }
